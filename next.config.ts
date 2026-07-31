baa5f49e65e4a4b903195f1819b54cb7a0c4012f
ArelioPalinoan <areliopalinoan@gmail.com>
2026-07-31 22:37:49 +0800

    feat: disable image optimization in next.config.ts for dev environment compatibility

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   allowedDevOrigins: ["10.10.8.220", "localhost", "127.0.0.1"],
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
